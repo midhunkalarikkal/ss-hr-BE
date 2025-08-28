a7a41e87c5e44628e51daddf4f96f2d62d23af95
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-08-28 17:09:21 +0530

    feat : admin creation feature added, eed to restructure the response

--- src/presentation/controllers/adminSettingsController.ts
+++ src/presentation/controllers/adminSettingsController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import { S3Client } from "@aws-sdk/client-s3";
+import { aws_s3Config } from "../../config/env";
 import { HandleError } from "../../infrastructure/error/error";
 import { CreateAdminZodSchema } from "../../infrastructure/zod/admin.zod";
+import { S3KeyGenerator } from "../../infrastructure/helper/generates3key";
+import { FileUploadService } from "../../infrastructure/service/fileUpload";
+import { SignedUrlService } from "../../infrastructure/service/generateSignedUrl";
 import { CreateAdminUseCase } from "../../application/adminUse-cases/adminSettingsUseCase";
 import { UserRepositoryImpl } from "../../infrastructure/database/user/userRepositoryImpl";
+import { SignedUrlRepositoryImpl } from "../../infrastructure/database/signedUrl/signedUrlRepositoryImpl";
+
+const s3Clien = new S3Client();
+const s3KeyGenerator = new S3KeyGenerator();
+const signedUrlRepositoryImpl = new SignedUrlRepositoryImpl();
+const signedUrlService = new SignedUrlService(aws_s3Config.bucketName, signedUrlRepositoryImpl);
 
 const userRepositoryImpl = new UserRepositoryImpl();
-const createAdminUseCase = new CreateAdminUseCase(userRepositoryImpl);
+const fileUploadService = new FileUploadService(s3Clien, signedUrlService, s3KeyGenerator);
+const createAdminUseCase = new CreateAdminUseCase(userRepositoryImpl, fileUploadService);
 
 export class AdminSettingsController {
     constructor(
@@ -17,7 +29,7 @@ export class AdminSettingsController {
     async createNewAdmin(req: Request, res: Response) {
         try {
             const validated = CreateAdminZodSchema.parse(req.body);
-            const result = await this.createAdminUseCase.execute({...validated, profileImage: req.file});
+            const result = await this.createAdminUseCase.execute({ ...validated, profileImage: req.file });
             res.status(201).json(result);
         } catch (error) {
             console.log("create admin controller error : ", error);
