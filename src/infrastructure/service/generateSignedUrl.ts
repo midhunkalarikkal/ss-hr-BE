8be9a2ee5eefd991761904a37acc2406cb2ddcb2
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-09-22 19:08:37 +0530

    refactor : env testing

--- src/infrastructure/service/generateSignedUrl.ts
+++ src/infrastructure/service/generateSignedUrl.ts
@@ -13,11 +13,8 @@ export class SignedUrlService {
   async generateSignedUrl(s3Key: string, expires: number = 172800): Promise<string> {
 
     try {
-
       console.log("generateSignedUrl service");
-      console.log("s3Key : ",s3Key);
       const existing = await this.signedUrlRepositoryImpl.findOneSignedUrl(s3Key);
-      console.log("existing : ",existing)
       if (existing && existing.expiresAt > new Date()) {
         return existing.url;
       }
@@ -31,9 +28,6 @@ export class SignedUrlService {
       const expiresAt = new Date(Date.now() + expires * 1000);
 
       const response = await this.signedUrlRepositoryImpl.findOneSignedUrlAndUpdate(s3Key, signedUrl, expiresAt);
-      console.log("findOneSignedUrlAndUpdate response : ",response);
-
-      console.log("signedUrl : ",signedUrl);
 
       return signedUrl;
     } catch (error) {
